e33c07a583b643ca73f98afc55fca494e66765ba
zhangmings <zhangming_Icciaf@163.com>
2017-10-12 00:04:07 +0800

    code-box

--- pages/js/index.js
+++ pages/js/index.js
@@ -1,61 +1,26 @@
-$(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-  $('.sidebar-background').css('background', 'url(./src/image/sidebar-4.jpg)');
-  $.ajax({
-    url: './pages/data/sidebar.json',
-    method: 'get',
-    dataType: 'json',
-    success: function(data) {
-      $(".sidebar-wrapper").sidebar({
-        data: data,
-        click:function(key){
-          if(key.url){
-            si.load(key);
-          }
+$('[data-toggle="tooltip"]').tooltip();
+$('.sidebar-background').css('background', 'url(./src/image/sidebar-4.jpg)');
+var $navTitle = $(".si-nav-title");
+$.ajax({
+  url: './pages/data/sidebar.json',
+  method: 'get',
+  dataType: 'json',
+  success: function(data) {
+    $(".sidebar-wrapper").sidebar({
+      data: data,
+      click:function(key){
+        if(key.url){
+          $navTitle.html(key.name)
+          si.load(key);
         }
-      });
-    }
-  });
-  $('#table').table({
-    columns: [{
-        field: 'id',
-        title: 'Item ID'
-    }, {
-        field: 'name',
-        title: 'Item Name'
-    }, {
-        field: 'price',
-        title: 'Item Price',
-        editable:{
-          type:'text',//text|textarea|select|date|checklist
-          emptytext:'价格',
-          mode:'popup',//popup:弹出层；inline行内
-          defaultValue:'100',//默认值
-          name:'item',
-          value:'213',
-          params:function(params) {
-            console.log(params)
-              params.a = 1;
-              return params;
-          },
-          url: '/post',
-          pk: 1,
-          send:'always',
-          validate:function(value) {
-              if($.trim(value) == '') {
-                  return '不可为空！';
-              }
-          }
-        }
-    }],
-    data: [{
-        id: 1,
-        name: 'Item 1',
-        price: '$1'
-    }, {
-        id: 2,
-        name: 'Item 2',
-        price: '$2'
-    }]
+      }
+    });
+  }
 });
+//code-box
+$(document).on("click",".code-expand-icon",function(){
+  var $codeBox = $(this).parents(".code-box");
+  var $codeWrapper= $codeBox.children(".code-box-wrapper");
+  $codeBox.toggleClass("expend");
+  $codeWrapper.toggleClass("expend");
 })
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './sass/sine.scss';
 import Sine from './js/sine';
 import {Sidebar,SidebarPlugin} from './js/sidebar';
 import {Textbox,TextboxPlugin} from './js/form/textbox';
-import {Form,FormPlugin} from './js/form';
+import {Form,FormPlugin} from './js/form/form';
 import './js/table';
 
 import Util from './libs/util';

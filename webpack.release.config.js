939020dba6ea92ff85a44d2d90c4cdd36fba4225
zhangmings <zhangming_Icciaf@163.com>
2018-05-23 11:14:49 +0800

    项目release版本

--- webpack.release.config.js
+++ webpack.release.config.js
@@ -3,25 +3,21 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.config.js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const WebpackBundleSizeAnalyzerPlugin = require('webpack-bundle-size-analyzer').WebpackBundleSizeAnalyzerPlugin;
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+const WebpackZipPlugin =require('webpack-zip-plugin');
 
 module.exports = merge(webpackBaseConfig, {
+  // 入口：要进行处理的实例（js）
+  entry: {
+    sine: './src/index.js',
+    bootstrap: './src/bootstrap.config.js'
+  },
   output: {
-    path: path.join(__dirname, './dist'),
-    // path: '/Users/zhangming/eclipse-workspace/zzsoft5.0/webapp/script/static/libs/sineui',
+    path: path.join(__dirname, './release'),
     publicPath: '../',
     filename: 'js/[name].min.js'
   },
   plugins: [
-    // new HtmlWebpackPlugin({
-    //   filename: 'index.html',
-    //   title: 'sineUI',
-    //   template: 'src/template/index.html',
-    //   inject:true
-    // }),
     new webpack.optimize.UglifyJsPlugin({
       output: {
         comments: false,  
@@ -30,19 +26,23 @@ module.exports = merge(webpackBaseConfig, {
         warnings: false
       }
     }),
+    
     new ExtractTextPlugin({
       filename: 'css/[name].min.css',
       allChunks: true
     }),
-    new WebpackBundleSizeAnalyzerPlugin('./plain-report.txt'),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new BundleAnalyzerPlugin()
     new CompressionPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
       test: /\.css$|\.js$|\.html$/,
       threshold: 0,
       minRatio: 1
+    }),
+    new WebpackZipPlugin({
+      initialFile: './release',  //需要打包的文件夹(一般为dist)
+      endPath: './static/release',  //打包到对应目录（一般为当前目录'./'）
+      zipName: 'sineui.zip' //打包生成的文件名
     })
-  ],
+  ]
 });

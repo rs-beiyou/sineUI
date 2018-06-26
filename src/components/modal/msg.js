5bb2e4b96efb483e2810ab689f179b24bd3f8a96
zhangmings <zhangming_Icciaf@163.com>
2018-06-26 14:12:41 +0800

    modal

--- src/components/modal/msg.js
+++ src/components/modal/msg.js
@@ -1,9 +1,10 @@
-import layer from 'libs/layer/layer.js';
 import 'libs/layer/theme/default/layer.css';
-$.extend({
 
+import layer from 'libs/layer/layer.js';
+
+$.extend({
   //提示层 0感叹号1正确2错误3问号4锁5哭脸6笑脸
   msg: function(message, obj, callback) {
     layer.msg(message, obj, callback);
-  },
+  }
 });

86fb5a4cbc4b3cc3aa700a456574f5298ca605ae
zhangmings <zhangming_Icciaf@163.com>
2018-08-14 17:00:09 +0800

    Sine.minxin

--- src/components/sine/mixin.js
+++ src/components/sine/mixin.js
@@ -1,4 +1,10 @@
 export default function (mixin) {
-  Object.assign(this.options, mixin || {});
+  Object.keys(mixin).forEach(item => {
+    if(Object.prototype.toString.call(this.options[item]) === '[object Array]'){
+      this.options[item].push(mixin[item]);
+      return true;
+    }
+    this.options[item] = mixin[item];
+  });
   return this;
 }

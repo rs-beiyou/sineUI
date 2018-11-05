91a41cf84287d05ed237bf49bde691c343316a54
zhangmings <zhangming_Icciaf@163.com>
2018-11-05 19:45:47 +0800

    1.2.8

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,18 +3,19 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const webpackBaseConfig = require('./webpack.config.js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const packageInfo = require('./package.json');
 
 module.exports = merge(webpackBaseConfig, {
   devtool: '#cheap-module-source-map',
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
-    filename: 'js/[name].min.js'
+    filename: `js/[name]-${packageInfo.version}.min.js`
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin({
-      filename: 'css/[name].min.css',
+      filename: `css/[name]-${packageInfo.version}.min.css`,
       allChunks: true
     })
   ]
